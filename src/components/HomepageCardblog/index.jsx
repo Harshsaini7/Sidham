e118a3fe76e2a7a3cc4ab51270e525bab93201fe
Harsh Saini <136721570+Harshsaini7@users.noreply.github.com>
2024-09-09 00:08:14 +0530

    Update index.jsx
    blog feature added

--- src/components/HomepageCardblog/index.jsx
+++ src/components/HomepageCardblog/index.jsx
@@ -1,64 +1,30 @@
 import React from "react";
 
-import { Button, Img, Line, Text } from "components";
+import { Img, Text } from "components";
 
-const HomepageCardblog = (props) => {
+const BlogDetailCardrecent = (props) => {
   return (
     <>
       <div className={props.className}>
-        <div className="flex flex-col gap-6 items-start justify-start w-full">
+        <div className="flex flex-row gap-[15px] items-center justify-start w-full">
           <Img
-            className="h-[400px] sm:h-auto object-cover w-full"
-            alt="rectangleEighteen"
-            src={props?.rectangleeighteen}
+            className="h-[70px] md:h-auto object-cover w-[70px]"
+            alt="image"
+            src={props?.image}
           />
-          <div className="flex flex-col gap-[19px] items-start justify-start w-full">
+          <div className="flex flex-1 flex-col gap-2 items-start justify-start w-full">
             <Text
-              className="text-2xl md:text-[22px] text-black-900 sm:text-xl tracking-[-0.50px] w-full"
-              size="txtRalewayBold24"
+              className="text-base text-black-900 w-full"
+              size="txtRalewayRomanBold16"
             >
-              {props?.whyshouldyouchoOne}
+              {props?.howtochoosea}
             </Text>
-            <div className="flex flex-col items-center justify-start w-full">
-              <div className="flex flex-row gap-[17px] items-center justify-start w-[416px] md:w-full">
-                <div className="flex flex-row gap-[5px] items-center justify-start w-auto">
-                  <Img
-                    className="h-[18px] w-[18px]"
-                    src="images/img_edit.svg"
-                    alt="edit"
-                  />
-                  <Text
-                    className="text-black-900 text-sm tracking-[-0.50px] w-auto"
-                    size="txtRubikRegular14Black900"
-                  >
-                    {props?.byadmin}
-                  </Text>
-                </div>
-                <Line className="bg-gray-500 h-[15px] w-px" />
-                <div className="flex flex-row gap-[5px] items-center justify-start w-auto">
-                  <Img
-                    className="h-[18px] w-[18px]"
-                    src="images/img_calendar.svg"
-                    alt="calendar"
-                  />
-                  <Text
-                    className="text-black-900 text-sm tracking-[-0.50px] w-auto"
-                    size="txtRubikRegular14Black900"
-                  >
-                    {props?.december102022}
-                  </Text>
-                </div>
-              </div>
-            </div>
             <Text
-              className="leading-[35.00px] max-w-[416px] md:max-w-full text-gray-500 text-sm tracking-[-0.50px]"
+              className="text-gray-500 text-sm tracking-[-0.50px] w-full"
               size="txtRubikRegular14"
             >
-              {props?.description}
+              {props?.december272021}
             </Text>
-            <Button className="border border-black-900 border-solid cursor-pointer font-rubik font-semibold leading-[normal] min-w-[139px] py-[11px] text-black-900 text-center text-lg tracking-[-0.50px]">
-              {props?.readMore}
-            </Button>
           </div>
         </div>
       </div>
@@ -66,18 +32,10 @@ const HomepageCardblog = (props) => {
   );
 };
 
-HomepageCardblog.defaultProps = {
-  rectangleeighteen: "images/img_rectangle18.png",
-  whyshouldyouchoOne: "Why should you choose good wood",
-  byadmin: "By Admin",
-  december102022: "December 10, 2022",
-  description: (
-    <>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum has been the industry&#39;s standard dummy text ever.
-    </>
-  ),
-  readMore: "Read More",
+BlogDetailCardrecent.defaultProps = {
+  image: "images/img_rectangle1480_120x120.png",
+  howtochoosea: "How to choose a chair at home",
+  december272021: "December 27, 2021",
 };
 
-export default HomepageCardblog;
+export default BlogDetailCardrecent;
